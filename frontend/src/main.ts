41223040b5542cda97e768a85b4ff1d9d51da7e3
João Paulo Freitas Queiroz <Joaop3595@gmail.com>
2024-02-28 11:54:09 -0300

    feat: add mdi icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-// Vuetify
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
